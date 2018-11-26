77d83efd0d9adc94c43df3aec6acab2387b1a42c
JulianChu <yulang.chu@gmail.com>
2018-11-26 21:45:13 +0100

    mock http test

---
+++ mockhttptest/mockhttptest_test.go
@@ -0,0 +1,26 @@
+package mockhttptest
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHttp(t *testing.T) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "{ \"status\": \"expected service response\"}")
+	}
+
+	req := httptest.NewRequest("GET", "https://www.google.com", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+}
